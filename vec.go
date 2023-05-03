8ace8951855d515474deaa689f0bdb3760d06120
erietz <ewrietz@gmail.com>
2023-05-03 11:37:53 -0500

    remove main()

--- vec.go
+++ vec.go
@@ -1,7 +1,5 @@
 package vec
 
-import "fmt"
-
 type Vec[T any] []T
 
 func (vec Vec[T]) Filter(fn func(t T) bool) Vec[T] {
@@ -28,15 +26,4 @@ func Map[X, Y any](vec Vec[X], fn func(x X) Y) Vec[Y] {
 		newVec = append(newVec, fn(v))
 	}
 	return newVec
-}
-
-func main() {
-	v1 := Vec[int]{1, 32, 23, 2432, 4324, 4, 34, 2, 3, 4, 2, 34, 234}
-	v2 := v1.Filter(func(i int) bool { return i == 2 })
-	v3 := v1.Map(func(i int) int { return i * 2 })
-	v4 := Map(v1, func(i int) string { return fmt.Sprintf("foo%d", i) })
-	fmt.Println(v1)
-	fmt.Println(v2)
-	fmt.Println(v3)
-	fmt.Println(v4)
 }
